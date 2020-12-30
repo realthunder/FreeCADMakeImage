abf8179ea64e9941f35fa0544f8c6e103b1098ac
Zheng, Lei <realthunder.dev@gmail.com>
2020-12-30 16:14:10 +0800

    Avoid '.' in branding minor version

--- conda/branding/asm3/install.sh
+++ conda/branding/asm3/install.sh
@@ -7,7 +7,7 @@ dst=$2
 cp $src/branding/branding.xml $dst/bin/
 if test $FMK_BUILD_DATE; then
     sed -i -e "s@_FC_VERSION_MAJOR_@${FMK_BUILD_DATE:0:4}@g" $dst/bin/branding.xml
-    sed -i -e "s@_FC_VERSION_MINOR_@${FMK_BUILD_DATE:4:2}.${FMK_BUILD_DATE:6}@g" $dst/bin/branding.xml
+    sed -i -e "s@_FC_VERSION_MINOR_@${FMK_BUILD_DATE:4:2}${FMK_BUILD_DATE:6}@g" $dst/bin/branding.xml
     sed -i -e "s@_FC_BUILD_DATE_@$FMK_BUILD_DATE@g" $dst/bin/branding.xml
 fi
 cp $src/branding/* $dst/
