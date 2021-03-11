e4cf94dfbbddb6233d7ce45a55398e9706e737d4
Zheng, Lei <realthunder.dev@gmail.com>
2021-03-11 11:25:33 +0800

    Update splash image

--- conda/branding/asm3/install.sh
+++ conda/branding/asm3/install.sh
@@ -7,7 +7,10 @@ dst=$2
 cp $src/branding/branding.xml $dst/bin/
 if test $FMK_BUILD_DATE; then
     sed -i -e "s@_FC_VERSION_MAJOR_@${FMK_BUILD_DATE:0:4}@g" $dst/bin/branding.xml
-    sed -i -e "s@_FC_VERSION_MINOR_@${FMK_BUILD_DATE:4:2}${FMK_BUILD_DATE:6}@g" $dst/bin/branding.xml
+    # trim leading zero
+    month=`printf %d ${FMK_BUILD_DATE:4:2}`
+    sed -i -e "s@_FC_VERSION_MINOR_@$month${FMK_BUILD_DATE:6}@g" $dst/bin/branding.xml
+    sed -i -e "s@_FC_VERSION_MINOR2_@${FMK_BUILD_DATE:4:2}.${FMK_BUILD_DATE:6}@g" $dst/bin/branding.xml
     sed -i -e "s@_FC_BUILD_DATE_@$FMK_BUILD_DATE@g" $dst/bin/branding.xml
 fi
 cp $src/branding/* $dst/
