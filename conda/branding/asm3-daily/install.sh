3e83602d1681bded61fe7a393561a6ba014e7ed9
Zheng, Lei <realthunder.dev@gmail.com>
2021-04-07 13:23:09 +0800

    Change branding installation

---
+++ conda/branding/asm3-daily/install.sh
@@ -0,0 +1,1 @@
+../asm3/install.sh
--- conda/branding/asm3/install.sh
+++ conda/branding/asm3/install.sh
@@ -4,7 +4,27 @@ set -e
 src=$1
 dst=$2
 
-cp $src/branding/branding.xml $dst/bin/
+if test -f $dst/MacOS/FreeCADCmd; then
+    bin=$dst/MacOS
+    cp -a $src/MacBundle/* $dst/
+    sed -i '' "s@_FC_BUNDLE_VERSION_@${FMK_BUILD_DATE:0:4}@g" $dst/Info.plist
+    mv $dst/MacOS/FreeCAD $dst/MacOS/FreeCADLink
+elif test -f $dst/bin/FreeCADCmd; then
+    bin=$dst/bin
+    rm -f $dst/../freecad_conda.desktop $dst/../freecad_conda.png
+    cp $src/AppDir/freecad_link.desktop $src/AppDir/freecad_link.png $dst/../
+    cp -a $src/icons $dst/share/
+    mv $dst/bin/FreeCAD $dst/bin/FreeCADLink
+elif test -f  $dst/bin/FreeCADCmd.exe; then
+    mv $dst/bin/FreeCAD.exe $dst/bin/FreeCADLink.exe
+else
+    echo failed to find bin directory
+    exit 1
+fi
+
+mkdir -p $dst/bin
+cp $src/branding/* $dst
+mv $dst/branding.xml $dst/bin
 if test $FMK_BUILD_DATE; then
     sed -i -e "s@_FC_VERSION_MAJOR_@${FMK_BUILD_DATE:0:4}@g" $dst/bin/branding.xml
     # trim leading zero
@@ -12,16 +32,6 @@ if test $FMK_BUILD_DATE; then
     sed -i -e "s@_FC_VERSION_MINOR_@$month${FMK_BUILD_DATE:6}@g" $dst/bin/branding.xml
     sed -i -e "s@_FC_VERSION_MINOR2_@${FMK_BUILD_DATE:4:2}.${FMK_BUILD_DATE:6}@g" $dst/bin/branding.xml
     sed -i -e "s@_FC_BUILD_DATE_@$FMK_BUILD_DATE@g" $dst/bin/branding.xml
+    rm -f "$dst/bin/branding.xml-e"
 fi
-cp $src/branding/* $dst/
-rm -f $dst/../freecad_conda.desktop $dst/../freecad_conda.png
-cp $src/AppDir/freecad_link.desktop $src/AppDir/freecad_link.png $dst/../
-
-if test -d $dst/share; then
-    cp -a $src/icons $dst/share/
-    mv $dst/bin/FreeCAD $dst/bin/FreeCADLink
-elif test -f $dst/bin/FreeCAD.exe; then
-    mv $dst/bin/FreeCAD.exe $dst/bin/FreeCADLink.exe
-fi
-
 
