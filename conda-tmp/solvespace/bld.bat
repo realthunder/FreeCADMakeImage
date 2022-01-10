c707199fc28e26e27a648f99e6332d206b07b3c2
Zheng, Lei <realthunder.dev@gmail.com>
2022-01-10 11:15:10 +0800

    Update solvespace package
    But not adding it for building as asm3 will now prompt user for
    installation

--- conda-tmp/solvespace/bld.bat
+++ conda-tmp/solvespace/bld.bat
@@ -4,14 +4,15 @@ cd build
 cmake -G "Ninja" ^
       -D BUILD_PYTHON:BOOL=ON ^
       -D CMAKE_BUILD_TYPE:STRING=Release ^
-      -D FREECAD_LIBPACK_USE:BOOL=OFF ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
       -D CMAKE_PREFIX_PATH:FILEPATH=%LIBRARY_PREFIX% ^
+      -D Python3_FIND_STRATEGY=LOCATION ^
       -D PYTHON_EXECUTABLE:FILEPATH=%PREFIX%/python ^
+      -D Python3_EXECUTABLE:FILEPATH=%PREFIX%/python ^
       ..
 
 if errorlevel 1 exit 1
 ninja _slvs
 if errorlevel 1 exit 1
-copy src/swig/python/_slvs.so %SP_DIR%
-copy src/swig/python/slvs.py %SP_DIR%
+copy "src\swig\python\_slvs.so" "%SP_DIR%"
+copy "src\swig\python\slvs.py" "%SP_DIR%"
--- conda-tmp/solvespace/build.sh
+++ conda-tmp/solvespace/build.sh
@@ -1,11 +1,11 @@
 mkdir -p build
 cd build
 
-cmake \
+cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_BUILD_TYPE=Release \
       -D BUILD_PYTHON=ON \
       ..
 
-make _slvs
+ninja _slvs -v -w dupbuild=warn
 cp src/swig/python/_slvs.so src/swig/python/slvs.py $SP_DIR
