7d44a451fd4b98c91a45dea1165715024a85cb53
Zheng, Lei <realthunder.dev@gmail.com>
2022-03-10 11:18:36 +0800

    Update conda fcitx-qt5

--- conda/fcitx-qt5/build.sh
+++ conda/fcitx-qt5/build.sh
@@ -1,11 +1,11 @@
 mkdir -p build
 cd build
 
-cmake \
+cmake -G "Ninja" \
       -D ENABLE_LIBRARY:BOOL=OFF \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       ..
 
-make install
+ninja install
