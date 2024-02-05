4cc3dbb110e8648e4bb6e64d723954f265eeaa43
Zheng, Lei <realthunder.dev@gmail.com>
2024-02-05 21:35:58 +0800

    Workaround 3dconnexion SDK installation failure on self hosted runner

--- conda/freecad_asm3/build.sh
+++ conda/freecad_asm3/build.sh
@@ -21,10 +21,13 @@ fi
 
 if [[ ${HOST} =~ .*darwin.* ]]; then
     # install space-mouse
-    curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-7-0_B564CC6A-6E81-42b0-82EC-418EA823B81A/3DxWareMac_v10-7-0_r3411.dmg'
-    hdiutil attach -readonly /tmp/3dFW.dmg
-    sudo installer -package /Volumes/3Dconnexion\ Software/Install\ 3Dconnexion\ software.pkg -target /
-    diskutil eject /Volumes/3Dconnexion\ Software
+    #
+    # sudo does not work in self hosted runner. Must manually install on runner host. So disable for now
+    #
+    # curl -o /tmp/3dFW.dmg -L 'https://download.3dconnexion.com/drivers/mac/10-7-0_B564CC6A-6E81-42b0-82EC-418EA823B81A/3DxWareMac_v10-7-0_r3411.dmg'
+    # hdiutil attach -readonly /tmp/3dFW.dmg
+    # sudo installer -package /Volumes/3Dconnexion\ Software/Install\ 3Dconnexion\ software.pkg -target /
+    # diskutil eject /Volumes/3Dconnexion\ Software
 
     # cmake_generator="Unix Makefiles"
     CMAKE_PLATFORM_FLAGS+=(-DFREECAD_USE_3DCONNEXION:BOOL=ON)
